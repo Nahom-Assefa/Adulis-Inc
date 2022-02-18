7cc428d7ccae4a2dd76bf41a3830dbcfc868914a
Nahom Assefa <nzenebe@gmail.com>
2022-02-18 12:00:08 -0600

    add-user fetch in progress

---
+++ public/assets/js/add-user.js
@@ -0,0 +1,56 @@
+// const submit = document.querySelector("#form-submit");
+// const firstName = document.querySelector("#fname");
+// const lastName = document.querySelector("#lname");
+// const email = document.querySelector("#email");
+// const phone = document.querySelector("#phone");
+// const payment = document.querySelector("#payment");
+
+const $userForm = document.querySelector("#registration-checkout");
+
+
+
+
+
+
+function userSubmit() {
+const firstName = $userForm.querySelector("#fname").value;
+const lastName = $userForm.querySelector("#lname").value;
+const email = $userForm.querySelector("#email").value;
+const phone = $userForm.querySelector("#phone").value;
+const payment = $userForm.querySelector("#payment").value;
+
+
+}
+
+
+
+
+
+
+
+$userForm.addEventListener("submit", userSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
