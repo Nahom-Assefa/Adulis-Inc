7f98ed973096b67876626106f562d78d3ae5b3f7
Nahom Assefa <nzenebe@gmail.com>
2022-02-18 12:19:59 -0600

    user post fetch

--- public/assets/js/add-user.js
+++ public/assets/js/add-user.js
@@ -7,50 +7,37 @@
 
 const $userForm = document.querySelector("#registration-checkout");
 
+function userSubmit(event) {
+  event.preventDefault();
 
+  const firstName = $userForm.querySelector("#fname").value;
+  const lastName = $userForm.querySelector("#lname").value;
+  const email = $userForm.querySelector("#email").value;
+  const phone = $userForm.querySelector("#phone").value;
+  const payment = $userForm.querySelector("#payment").value;
 
+  if (!firstName || !lastName || !email || !phone || !payment) {
+    return;
+  }
 
+  const formData = { firstName, lastName, email, phone, payment };
 
-
-function userSubmit() {
-const firstName = $userForm.querySelector("#fname").value;
-const lastName = $userForm.querySelector("#lname").value;
-const email = $userForm.querySelector("#email").value;
-const phone = $userForm.querySelector("#phone").value;
-const payment = $userForm.querySelector("#payment").value;
-
-
+  fetch("api/users", {
+    method: "Post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((postResponse) => {
+      alert("User created successfully!");
+      console.log(postResponse);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-
-
-
-
-
-
 $userForm.addEventListener("submit", userSubmit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
