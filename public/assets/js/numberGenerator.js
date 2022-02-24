ca5a2af6121e1944f8ce468b8c23933715025fb6
Nahom Assefa <nzenebe@gmail.com>
2022-02-24 11:25:52 -0600

    final MVP push

--- public/assets/js/numberGenerator.js
+++ public/assets/js/numberGenerator.js
@@ -1,5 +1,5 @@
-
 const randomPrice = Math.floor(Math.random() * 700);
 
-const modalNumber = document.querySelector('.sell-card').children[2].textContent = `$${randomPrice}`;
-
+const modalNumber = (document.querySelector(
+  ".sell-card"
+).children[2].textContent = `$${randomPrice}`);
